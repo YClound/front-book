00b8a327881b2d474b6e9cde056241b66f3578df
yanan.g055 <yanan.g055@shulidata.com>
2018-01-15 15:19:34 +0800

    增加文件夹

---
+++ authcall-web/css-js/gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const less = require('gulp-less');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('live', function () {
+    return watch('./common/less/**.less', function () {
+        gulp.src('./common/less/**.less')
+            .pipe(less())
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions', 'Android >= 4.0', 'iOS 7', 'last 3 Safari versions', '>5%', 'ie 6-8'],
+                cascade: false, //是否美化属性值 默认：true 像这样
+            }))
+            .pipe(gulp.dest('./common/css'))
+    })
+})
