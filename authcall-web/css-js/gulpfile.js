1859a4cdf0e20ff4c4401027d0b3dc9e3da7fc08
yanan.g055 <yanan.g055@shulidata.com>
2018-01-15 17:11:13 +0800

    css样式

--- authcall-web/css-js/gulpfile.js
+++ authcall-web/css-js/gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const watch = require('gulp-watch');
 const less = require('gulp-less');
 const autoprefixer = require('gulp-autoprefixer');
+const cssMin = require('gulp-minify-css');
 
 gulp.task('live', function () {
     return watch('./common/less/**.less', function () {
@@ -11,6 +12,7 @@ gulp.task('live', function () {
                 browsers: ['last 2 versions', 'Android >= 4.0', 'iOS 7', 'last 3 Safari versions', '>5%', 'ie 6-8'],
                 cascade: false, //是否美化属性值 默认：true 像这样
             }))
+            //.pipe(cssMin())
             .pipe(gulp.dest('./common/css'))
     })
 })
